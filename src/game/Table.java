35e8f04f6f75798c8600b6e82060e3c9d9506753
hfw <foest88@gmail.com>
2011-04-03 14:29:42 -0700

    Did a little commenting.

--- src/game/Table.java
+++ src/game/Table.java
@@ -25,6 +25,9 @@ public class Table {
 	Vector<Vector<Action>> HH=new Vector<Vector<Action>>(4);
 
 	public Table(){
+		
+		//Sets the 4 rounds: pre-flop, flop, turn and river. Each round has enough
+		//space for 100 actions.
 		for(int i=0;i<4;i++){
 			HH.add(i, new Vector<Action>(100));
 		}
@@ -36,6 +39,7 @@ public class Table {
 	
 	public void run(int numHands){
 		
+		
 		initPlayerCount();
 		
 		for(int i=0;i<numHands; i++){
@@ -46,11 +50,13 @@ public class Table {
 		
 	}
 	
+	//Plays an entire hand - pre-flop, flop, turn, and river.
 	public void playHand(){
 		//seats[(button+1)%10];
-		resurrectPlayers();
-		actionComplete=false;
 		
+		
+		resurrectPlayers();		
+		actionComplete=false;
 		advanceButton();
 		postSB();
 		advanceAction();
@@ -66,6 +72,7 @@ public class Table {
 		
 	}
 	
+	//Sets all plyers live field to true, meaning they can act again.
 	private void resurrectPlayers() {
 		for(int i=0;i<10;i++){
 			if(seats[i]!=null){
@@ -74,7 +81,8 @@ public class Table {
 		}
 		
 	}
-
+	
+	//Returns the index of the next player who has a live=true field.
 	private int findNextLivePlayer(int index){
 		
 		index++;
@@ -84,6 +92,7 @@ public class Table {
 		return index;
 	}
 	
+	//Calculates the number of players and sets playerCount to the result.
 	private void initPlayerCount() {
 		for(int i=0;i<10;i++){
 			if(seats[i]!=null)
@@ -93,6 +102,7 @@ public class Table {
 	}
 
 	
+	//Returns true if the round is over, otherwise returns false.
 	private Boolean isActionComplete(){
 		if(livePlayers==1)
 			return true;
@@ -102,6 +112,9 @@ public class Table {
 		return false;
 	}
 	
+	//Forces the player at index toAct to post the big blind.
+	
+	//TODO Does seats[toAct] need to be seats[toAct-1]?
 	private void postBB() {
 		HH.get(round).add(new Action(seats[toAct], 4, bb));
 		seats[toAct].setStack(-bb);
@@ -109,18 +122,21 @@ public class Table {
 		
 	}
 
+	//Forces the player at index toAct to post the small blind.
 	private void postSB(){
 		HH.get(round).add(new Action(seats[toAct], 4, sb));
 		seats[toAct].setStack(-sb);
 		setPot(sb);
 	}
 	
+	//Moves the button forward one player, skips null seats.
 	private void advanceButton(){
 		while(seats[button]==null){
 		button=(button+1)%10;
 		}
 	}
 	
+	//Sets toAct to the next player, skips null/"dead" players.
 	private void advanceAction(){
 		
 		do{
@@ -128,14 +144,17 @@ public class Table {
 		} while(seats[toAct]==null ||!seats[toAct].isLive());
 	}
 	
+	//Adds d to the pot.
 	private void setPot(double d){
 		pot+=d;
 	}
 	
+	//Adds a to the list of actions.
 	public void update(Action a){
 		HH.get(round).add(a);
 	}
 	
+	//Adds a new player to the table if the seat is open.
 	public void addPlayer(Player p, int seat){
 		if(seats[seat-1]==null){
 			seats[seat-1]=p;
